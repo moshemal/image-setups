d42ac915ed4af1c26b3ea01360424b2ee60b8ef8
Moshe Malka <moshemal@gmail.com>
2018-07-08 02:15:09 +0300

    add mysql

--- nodejs-mysql.sh
+++ nodejs-mysql.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+./packages.sh
+
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+sudo apt-get install -y  mysql-server
+mysql_secure_installation
