1dd88c7f9d75190b9afc9154b655cb729146716b
Moshe Malka <moshemal@gmail.com>
2019-03-18 21:44:54 +0200

    Update docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "do you have docker installed?"
-./docker.sh
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
